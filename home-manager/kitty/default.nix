f868d3e717a27652e36b1b56c47bd09fef621a0a
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-01-24 04:05:54 +0000

    kitty: Refactor config to nix

--- home-manager/kitty/default.nix
+++ home-manager/kitty/default.nix
@@ -1,8 +1,19 @@
 { config, pkgs, ... }:
 
 {
+    programs.kitty = {
+        enable = true;
+        font = {
+            name = "JetBrainsMono Nerd Font";
+            size = 10;
+        };
+        settings = {
+            enable_audio_bell = false;
+            include = "gruvbox-material-dark-hard.conf";
+        };
+    };
+
     home.file = {
-        ".config/kitty/kitty.conf".source = ./kitty.conf;
-        ".config/kitty/current-theme.conf".source = ./current-theme.conf;
+        ".config/kitty/gruvbox-material-dark-hard.conf".source = ./gruvbox-material-dark-hard.conf;
     };
 }
