e3a05e99564b5dca5088ea4bbeab24bcd99cb41f
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-01-24 02:33:58 +0000

    bat: Refactor config to nix

--- home-manager/bat/default.nix
+++ home-manager/bat/default.nix
@@ -1,8 +1,22 @@
 { config, pkgs, ... }:
 
 {
-    home.file = {
-        ".config/bat/themes/catppuccin-macchiato.tmTheme".source = ./themes/catppuccin-macchiato.tmTheme;
-        ".config/bat/config".source = ./config;
+    programs.bat = {
+        enable = true;
+        themes = {
+            gruvbox = {
+                src = pkgs.fetchFromGitHub {
+                    owner = "peaceant";
+                    repo = "gruvbox";
+                    rev = "e3db74d0e5de7bc09cab76377723ccf6bcc64e8c";
+                    sha256 = "gsk3qdx+OHMvaOVOlbTapqp8iakA350yWT9Jf08uGoU=";
+                };
+                file = "gruvbox.tmTheme";
+            };
+        };
+        config = {
+            theme = "gruvbox";
+            style = "plain";
+        };
     };
 }
