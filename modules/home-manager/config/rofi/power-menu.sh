ac66a11212930c337ea66a5d83367be9f5049494
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-01-27 23:15:35 +0800

    rofi: Refactor config to nix and add plugins

---
+++ modules/home-manager/config/rofi/power-menu.sh
@@ -0,0 +1,34 @@
+# Power menu options
+options="Shutdown\nReboot\nLog Out\nLock\nSuspend\nHibernate\nCancel"
+
+# Show options in Rofi
+chosen=$(echo -e "$options" | rofi -dmenu -p "Power Menu" -theme-str '@import "gruvbox-material"')
+
+case "$chosen" in
+    "Shutdown")
+        systemctl poweroff
+        ;;
+    "Reboot")
+        systemctl reboot
+        ;;
+    "Log Out")
+        # Depending on your session manager (like i3, sway, etc.), modify this command.
+        pkill -KILL -u $USER
+        ;;
+    "Lock")
+        # Locks the screen using your preferred method (e.g., i3lock or betterlockscreen)
+        betterlockscreen -l
+        ;;
+    "Suspend")
+        systemctl suspend
+        ;;
+    "Hibernate")
+        systemctl hibernate
+        ;;
+    "Cancel")
+        exit 0
+        ;;
+    *)
+        exit 1
+        ;;
+esac
