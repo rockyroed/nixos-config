780d935b64f99fda5c6ae1c6bd17ecaced0a4dc8
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-01-28 13:48:30 +0800

    bin: Create spotify_cava.sh

--- modules/home-manager/config/bin/default.nix
+++ modules/home-manager/config/bin/default.nix
@@ -4,5 +4,6 @@
     home.file = {
         ".bin/headphone_audio.sh".source = ./headphone_audio.sh;
         ".bin/xborders.sh".source = ./xborders.sh;
+        ".bin/spotify_cava.sh".source = ./spotify_cava.sh;
     };
 }
