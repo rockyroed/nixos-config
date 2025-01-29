ee4231d69acca854a09bf2cadca0fb7420b9da37
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-01-29 14:59:14 +0800

    lint: First commit

--- modules/home-manager/config/i3/volume_up.sh
+++ modules/home-manager/config/i3/volume_up.sh
@@ -2,7 +2,7 @@ current_volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | sed '
 new_volume=$((current_volume + 5))
 
 if [ $new_volume -gt 100 ]; then
-    new_volume=100
+     new_volume=100
 fi
 
 pactl set-sink-volume @DEFAULT_SINK@ ${new_volume}%
