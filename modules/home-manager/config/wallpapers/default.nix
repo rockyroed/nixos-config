d81559434e247cba033a0d28e4d2231ce57c74d6
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-06 10:27:23 +0800

    wallpapers: Include new wallpaper to home.file

--- modules/home-manager/config/wallpapers/default.nix
+++ modules/home-manager/config/wallpapers/default.nix
@@ -3,5 +3,6 @@
 {
     home.file = {
         ".wallpapers/gruv-limits.png".source = ./gruv-limits.png;
+        ".wallpapers/this-wallpaper-is-not-available.png".source = ./this-wallpaper-is-not-available.png;
     };
 }
