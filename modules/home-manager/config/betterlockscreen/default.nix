3c7f7bbc2d75f13203a14676f3d6c042f05ec65b
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-06 12:46:30 +0800

    betterlockscreen: Lock on suspend

--- modules/home-manager/config/betterlockscreen/default.nix
+++ modules/home-manager/config/betterlockscreen/default.nix
@@ -9,5 +9,26 @@
       ];
     };
 
+    systemd.user.services.betterlockscreen = {
+      Unit = {
+        Description = "Better screen locker";
+        Documentation = [ "man:betterlockscreen(1)" ];
+        ConditionPathExists = "/var/run/systemd/sessions";
+      };
+
+      Service = {
+        ExecStart = ''
+          if systemctl is-active sleep.target; then
+            ${pkgs.betterlockscreen}/bin/betterlockscreen -l dim
+          fi
+        '';
+        Restart = "always";
+      };
+
+      Install = {
+        WantedBy = [ "sleep.target" ];
+      };
+    };
+
     home.file.".config/betterlockscreen/betterlockscreenrc".source = ./config/betterlockscreenrc;
 }
