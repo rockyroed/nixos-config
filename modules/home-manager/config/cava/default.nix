a81e528f48485bdd77efcd721b978d579fd7fc38
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-10 19:57:19 +0800

    cava: Modify some config

--- modules/home-manager/config/cava/default.nix
+++ modules/home-manager/config/cava/default.nix
@@ -5,7 +5,8 @@
         enable = true;
         settings = {
             color = {
-                gradient = true;
+                gradient = "1";
+                gradient_count = "5";
                 gradient_color_1 = "#a9b665";
                 gradient_color_2 = "#89b482";
                 gradient_color_3 = "#d8a657";
@@ -13,9 +14,16 @@
                 gradient_color_5 = "#ea6962";
             };
             output = {
+                channels = "mono";
+                mono_option = "average";
                 vertex_shader = "pass_through.vert";
                 fragment_shader = "bar_spectrum.frag";
             };
+            smoothing = {
+                monstercat = "1";
+                waves = "1";
+                noise_reduction = "0";
+            }
         };
     };
     
