9f3102134894af628410a549a5b5503bf63e5ed1
Christian Roed Boyles <christianroedpboyles@protonmail.com>
2025-02-12 14:08:07 +0800

    polybar: Remove underline in systray

--- modules/home-manager/config/polybar/nix/tray.nix
+++ modules/home-manager/config/polybar/nix/tray.nix
@@ -1,13 +1,9 @@
-{...}: let
-  purple = "#d3869b";
-in {
+{...}: {
   services.polybar.config = {
     "module/systray" = {
       type = "internal/tray";
 
       tray-spacing = "4pt";
-
-      format-underline = "${purple}";
     };
   };
 }
