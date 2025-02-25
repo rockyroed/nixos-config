41bacc2816e5a2eee8a9dd6dc75f9e1f2b069706
rockyroed <roed-imp.vo97b@slmail.me>
2025-02-25 22:24:02 +0800

    nixos: Add gnome auth agent

--- hosts/desktop/desktop.nix
+++ hosts/desktop/desktop.nix
@@ -32,6 +32,23 @@
   '';
   security.polkit.enable = true;
 
+  # Gnome auth agent
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
   # Hostname
   networking.hostName = "roed-nixos";
 
