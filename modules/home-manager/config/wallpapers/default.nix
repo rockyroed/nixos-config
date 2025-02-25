e40835c1acc82950a997f79175308cae34af4402
rockyroed <roed-imp.vo97b@slmail.me>
2025-02-25 20:07:55 +0800

    wallpapers: Source new

--- modules/home-manager/config/wallpapers/default.nix
+++ modules/home-manager/config/wallpapers/default.nix
@@ -2,5 +2,7 @@
   home.file = {
     ".wallpapers/gruv-limits.png".source = ./images/gruv-limits.png;
     ".wallpapers/this-wallpaper-is-not-available.png".source = ./images/this-wallpaper-is-not-available.png;
+    ".wallpapers/gravity_falls.webp".source = ./images/gravity_falls.webp;
+    ".wallpapers/the-regular-show.png".source = ./images/the-regular-show.png;
   };
 }
